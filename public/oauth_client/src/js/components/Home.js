1f110df81e8422ee555b71deee5ad85679d1f77b
King <d3po.techengineer@gmail.com>
2020-03-24 15:09:44 -0400

    add link to header home btn to return to location.origin remove oauth_server and oauth_client for 2 new separate repos

--- public/oauth_client/src/js/components/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default () => {
-  return (
-    <div>
-      Welcome to our home page!
-    </div>
-  )
-}// default
--- public/oauth_client/src/js/paths/index.js
+++
@@ -1,4 +0,0 @@
-export const HOME_PATH = '/auth';
-export const SIGN_UP_PATH = '/auth/signup';
-export const SIGN_IN_PATH = '/auth/signin';
-export const DASHBOARD_PATH = '/auth/dashboard';
